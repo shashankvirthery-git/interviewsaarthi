940ca56af2f00f67b8b9d4abe11d3c88e8561bef
Sashank <shashankpandit707@gmail.com>
2026-04-19 22:08:17 +0530

    connected backend final

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api'
+const API_URL = 'https://interviewsaarthi.onrender.com/api'
 const api = axios.create({ baseURL: API_URL })
 
 api.interceptors.request.use((config) => {
